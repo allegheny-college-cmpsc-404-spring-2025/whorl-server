f9f8e46382e4d3604ce87b9440fadf9b31332a02
Rebekah Rudd <rudd01@allegheny.edu>
2025-03-27 16:18:12 -0400

    remove the log exportation again

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "whorl-server",
     interpreter: "./.venv/bin/python",
     script: 'src/manage.py',
-    args:'runserver 0.0.0.0:8080 --exclude "*.log"',
+    args:'runserver 0.0.0.0:8080',
     watch: '.'
   }],
 };

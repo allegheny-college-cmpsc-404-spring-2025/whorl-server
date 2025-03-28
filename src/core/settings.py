6f864bc89992f625044b408a1b2fe62944cddd97
Rebekah Rudd <rudd01@allegheny.edu>
2025-03-28 10:14:37 -0400

    call the python file that performs log saving

--- src/core/settings.py
+++ src/core/settings.py
@@ -372,49 +372,6 @@ LOGGING = {
     },
 }
 
-
-# Rotate through the file logs by compressing and archiving old log files.
-# Get the current date to use in the archived log file name.
-current_date = datetime.date.today()
-
-logging.basicConfig(filename='scheduler.log')
-schedule_logger = logging.getLogger('schedule')
-schedule_logger.setLevel(level=logging.DEBUG)
-
-# # find the different file paths
-MODULE_PATH = pathlib.Path(__file__).parent.parent.resolve()
-absolute_path_to_logs = os.path.join(MODULE_PATH, "logs")
-file_path = os.path.join(absolute_path_to_logs, "debug.log")
-new_file_path = os.path.join(MODULE_PATH, "past_logs", str(current_date) + ".gz")
-
-def compress_old_logs():
-    """
-    Moves and compresses rotated log files into 'past_logs' directory."
-    """
-    # Compress log file
-    with open(file_path, "rb") as f_in, gzip.open(new_file_path, "wb") as f_out:
-        print("this works: ", current_date)
-        shutil.copyfileobj(f_in, f_out)
-
-    # Clear all the files in src/logs log file after compression
-    for file in os.listdir(absolute_path_to_logs):
-        logs_file_path = os.path.join(absolute_path_to_logs, file)
-        if not os.path.exists(logs_file_path):
-            open(file, 'w').close()
-            print(f"File added: {logs_file_path}")
-        open(logs_file_path, 'r+').truncate(0)
-
-    print(f"Compressed and moved: {new_file_path}")
-
-def job():
-    print("Executing the scheduled job")
-    # Add your task logic here
-    # return "Job completed"
-    compress_old_logs()
-
-# schedule.every(1).minutes.do(job)
-schedule.every().day.at("00:00").do(job)
-
-# while True:
-#     schedule.run_pending()
-    # time.sleep(1)
+# call the file that performs log saving
+os.system("python core/python_scheduler.py")
+# print("this is running")

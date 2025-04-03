fea00230b5b1848986a805a72625ea7b1f28bf50
hemanialaparthi <alaparthi01@allegheny.edu>
2025-04-03 15:38:08 -0400

    fix(views.py): add will's docstrings for views

--- src/omnipresence/views.py
+++ src/omnipresence/views.py
@@ -10,11 +10,13 @@ from rest_framework.mixins import UpdateModelMixin
 from omnipresence.models import OmnipresenceModel
 from omnipresence.serializer import OmnipresenceSerializer
 
-class OmnipresenceView(GenericAPIView):
 
+class OmnipresenceView(GenericAPIView):
+    """API view to retrieve and create OmnipresenceModel instances."""
     queryset = OmnipresenceModel.objects.all()
 
     def get(self, request, *args, **kwargs):
+        """Retrieve character information based on 'charname' query parameter."""
         character = OmnipresenceModel.objects.filter(
             charname = request.GET.get('charname')
         ).values('pk', 'username', 'charname', 'working_dir', 'is_active')
@@ -27,6 +29,7 @@ class OmnipresenceView(GenericAPIView):
         return HttpResponse(json.dumps(dict()), status = 200, content_type = 'application/json')
 
     def post(self, request, *args, **kwargs):
+        """Create a new OmnipresenceModel instance with the provided data."""
         data = {
             'username': request.data.get('username'),
             'charname': request.data.get('charname'),
@@ -38,11 +41,14 @@ class OmnipresenceView(GenericAPIView):
             return Response(status = 201)
         return Response(serializer.errors, status = 400)
 
+
 class OmnipresenceActiveView(GenericAPIView):
+    """API view to retrieve active OmnipresenceModel instances."""
 
     queryset = OmnipresenceModel.objects.all()
 
     def get(self, request, *args, **kwargs):
+        """ Retrieve all active characters. """
         actives = OmnipresenceModel.objects.filter(
             is_active = True
         ).values('username','charname')
@@ -53,6 +59,7 @@ class OmnipresenceActiveView(GenericAPIView):
         )
 
     def post(self, request, *args, **kwargs):
+        """Retrieve active characters based on the current working directory."""
         local_actives = OmnipresenceModel.objects.filter(
             working_dir = request.data.get('cwd')
         ).values('charname')
@@ -62,10 +69,12 @@ class OmnipresenceActiveView(GenericAPIView):
             content_type = 'application/json'
         )
 
-class OmnipresenceUpdateView(GenericAPIView, UpdateModelMixin):
 
+class OmnipresenceUpdateView(GenericAPIView, UpdateModelMixin):
+    """API view to update OmnipresenceModel instances."""
     queryset = OmnipresenceModel.objects.all()
     serializer_class = OmnipresenceSerializer
 
     def patch(self, request, *args, **kwargs):
+        """Partially update an OmnipresenceModel instance."""
         return self.partial_update(request, *args, **kwargs)

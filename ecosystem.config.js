0b882e400607b4806ecb997bc94adabd57321ea0
Poiuy7312 <pjs075637@gmail.com>
2025-04-03 15:07:34 -0400

    chore: changed prom directory to location on dev server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-  apps : [{
+  apps: [{
     name: "whorl-server",
     interpreter: "./.venv/bin/python",
     script: 'src/manage.py',
-    args:'runserver 0.0.0.0:8080',
+    args: 'runserver 0.0.0.0:8080',
     watch: '.'
-  }],
+  },
+  {
+    name: "prometheus",
+    script: "usr/bin/prometheus",
+    args: "--config.file=prometheus.yml"
+  }
+  ]
 };

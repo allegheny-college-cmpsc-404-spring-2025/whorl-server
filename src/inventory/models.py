a6a126162aa9c092cf068edc5e1e961044ae2344
Aidan Dyga <aidan.dyga@gmail.com>
2025-04-15 13:41:38 -0400

    fix: reverting to remove backpack class

--- src/inventory/models.py
+++ src/inventory/models.py
@@ -1,7 +1,5 @@
 import pgtrigger
-import pickle
 from django.db import models
-from .backpack import Backpack
 
 @pgtrigger.register(
     pgtrigger.Trigger(
@@ -40,16 +38,16 @@ from .backpack import Backpack
 class Inventory(models.Model):
     item_owner = models.ForeignKey(
         'omnipresence.OmnipresenceModel',
-        on_delete=models.DO_NOTHING,
-        default=0
+        on_delete = models.DO_NOTHING,
+        default = 0
     )
-    item_name = models.CharField(max_length=255)
+    item_name = models.CharField(max_length = 255)
     item_qty = models.FloatField(default=1.0)
     item_weight = models.FloatField(default=1.0)
     item_bulk = models.FloatField(default=1.0)
-    item_version = models.CharField(max_length=255, default="1.0.0")
-    item_consumable = models.BooleanField(default=False)
-    item_bytestring = models.BinaryField(default=b'\x08', editable=True)
+    item_version = models.CharField(max_length = 255, default = "1.0.0")
+    item_consumable = models.BooleanField(default = False)
+    item_bytestring = models.BinaryField(default = b'\x08', editable = True)
 
     def __str__(self):
         return self.item_name
@@ -60,21 +58,3 @@ class Inventory(models.Model):
         for field in fields:
             result[field.name] = getattr(self, field.name)
         return result
-
-    def is_container(self):
-        """Check if the item is a container (e.g., Backpack)."""
-        return self.item_name.lower() == "backpack"
-
-    def get_container_items(self):
-        """Retrieve items contained in the container."""
-        if self.is_container():
-            return Backpack.deserialize(self.item_bytestring).items
-        return []
-
-    def add_to_container(self, item):
-        """Add an item to the container."""
-        if self.is_container():
-            backpack = Backpack.deserialize(self.item_bytestring)
-            backpack.add_item(item)
-            self.item_bytestring = backpack.serialize()
-            self.save()
